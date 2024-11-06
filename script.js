fce4df4596f724252d4621156c72054956684c41
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-11-06 18:04:47 +0530

    feat: added a example of lan and lon (San Francisco) and sucsessfully recived the json file

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 //going to use Ambee's Air Quality API
 const apiKey = '23b1363ce46188a601ed51900d4a058c';
-const apiurl = 'http://api.openweathermap.org/data/2.5/air_pollution';
+const apiurl = 'https://api.openweathermap.org/data/2.5/air_pollution';
 
 //url = apiurl?lat={lat}&lon={lon}&appid={apikey}
 
 async function getairqualityindex(lat,lon) {
     try {
-        const response = await fetch(`apiurl?lat=${lat}&lon=${lon}&appid=${apikey}`) ;
-        const data = response.json();
+        const response = await fetch(`${apiurl}?lat=${lat}&lon=${lon}&appid=${apiKey}`);
+        const data = await response.json();
         console.log(data);
     }
 
+    catch(error){
+        console.log("error")
+    }
 
-    
 }
+
+//Example coordinates for San Ftancisco
+const lan = 37.7749;
+const lon = -122.4194;
+
+
+getairqualityindex(lan,lon);
