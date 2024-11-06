8b97047faa20a543ddd2602b12ed937e636632b2
prathoseraaj V <146630697+prathoseraaj0312@users.noreply.github.com>
2024-11-06 17:00:42 +0530

    feat: added api and url

---
+++ script.js
@@ -0,0 +1,10 @@
+//going to use Ambee's Air Quality API
+const apiKey = '23b1363ce46188a601ed51900d4a058c';
+const apiurl = 'http://api.openweathermap.org/data/2.5/air_pollution';
+
+//url = apiurl?lat={lat}&lon={lon}&appid={apikey}
+
+async function getairqualityindex(lat,lon) {
+    
+    
+}
